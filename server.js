b610a623881e3a22b2a14347b41a0c8bd40b38d5
balbini <balbini91@gmail.com>
2019-07-16 05:18:26 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema/schema");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("listening");
+});
